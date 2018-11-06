cdbcfdef3ecfdccc83612aa78f80040e8d0becea
jordanavi <jordanavilela01@gmail.com>
2018-11-06 21:43:05 -0200

    3 trabalhos de layout - Placas - Calculadora e Cardapio

---
+++ ex1layoutActivity/app/src/main/java/com/example/jorda/layoutactivity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jorda.layoutactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
